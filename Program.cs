05acac8a485da2b3c6badb353ec089706eeb2483
Bob Hildreth <bobh@thehildreths.com>
2019-07-09 17:39:33 -0700

    working code now

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,31 +12,46 @@ namespace SVMPredict
     {
         // Using libsvm library from Nicolas Panel https://github.com/nicolaspanel/libsvm.net
         // Currently unsupported
+
+        // from Grid search Lexie's 982 C = 2.0 g =  0.001953125 rate= 67.2098
+        static double C = 2.0;
+        static double gamma = 0.001953125;
+
         static void Main(string[] args)
         {
             if (args.Length <= 1)
             {
-                Console.WriteLine("Usage: PredictSVM <modelfile> <test file> ");
+                Console.WriteLine("Usage: PredictSVM <trainfile> <test file> ");
                 System.Environment.Exit(0);
             }
 
-            string modelfile = args[0];
+            string trainfile = args[0];
             string testfile = args[1];
-            double C = 1;
-            C_SVC svm;
+            Console.WriteLine("Trainfile = {0}", trainfile);
+            Console.WriteLine("testfile = {0}", testfile);
             
+                        
             var predfile = ProblemHelper.ReadProblem(testfile);
-            svm = new C_SVC(modelfile);
-            predfile = ProblemHelper.ReadProblem(testfile);
-
-            for (int i = 1; i < predfile.l; i++)
+            var svm = new C_SVC(trainfile, KernelHelper.RadialBasisFunctionKernel(gamma), C);
+            var cva = svm.GetCrossValidationAccuracy(5);
+            int i = 0;
+            int total = 1;
+                        
+            for ( i = 0; i < predfile.l; i++)
             {
-                var x = predfile.x[i]; // I assume that this gets one x vector sample
-                var predict = svm.Predict(x);
-
+                var x = predfile.x[i]; // x is the ith vector sample
+                var expectedValue = predfile.y[i];
+                var predictedValue = svm.Predict(x); // Make label prediciton 
+                if (predictedValue == expectedValue)
+                {
+                    total++;
+                }
+                
             }
-             
-            
+            Console.WriteLine("math {0}", 15d / 40d);
+            double result = ((double) total /(double) i);
+            Console.WriteLine("total samples {0}, Crossvalidation Accuracy {1}%",total, Math.Round(cva*100,2));
+            Console.WriteLine("Prediction accuracy = {0}%", Math.Round(result*100,2) );
             
         }
     }
