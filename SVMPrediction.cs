f3d0c623af5089abf029260311e341037b824fe0
Bob Hildreth <bobh@thehildreths.com>
2019-07-11 14:34:04 -0700

    prediction function added

---
+++ SVMPrediction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using libsvm;
+
+
+
+namespace PredictionRoutines
+{
+    class Predict 
+    {
+        
+         static public double Predict_y(string inputfile,C_SVC svm)
+        {
+            int i;
+            double total = 1;
+            var predfile = ProblemHelper.ReadProblem(inputfile);
+            double expectedValue = 0;
+
+            for (i = 0; i < predfile.l; i++)
+            {
+                var x = predfile.x[i]; // x is the ith vector sample
+                expectedValue = predfile.y[i];
+                var predictedValue = svm.Predict(x); // Make label prediciton 
+                if (predictedValue == expectedValue) // Compare the predicited with the known value and calculate accruacty
+                {
+                    total++;
+                }
+                               
+            }
+            return total;
+
+        }
+        
+    }
+}
